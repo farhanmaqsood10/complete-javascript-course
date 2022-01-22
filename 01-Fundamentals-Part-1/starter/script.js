3f916c8a1cd04df7108184b74c01abbf3e608a08
Farhan <farhanmaqsood10@hotmail.com>
2022-01-22 22:35:18 +0500

    first-commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,125 @@
+// let values = 43+43+43+4-5;
+// console.log(values);
+
+// const markWeight = 78;
+// const markHeight = 1.69;
+
+// const johnWeight = 92;
+// const johnHeight = 1.95;
+
+// console.log(johnWeight / johnHeight **2);
+// console.log(markWeight / markHeight **2);
+
+// const markHeightBMI = true;
+
+// if(markHeightBMI){
+//     console.log(`prinnt ${markHeightBMI}`)
+// }
+// else{
+//     'john body mass is smaller then mark'
+// }
+// if (js === 'Farhan') alert('js is fun');
+// 45+8+23-10;
+
+
+/*
+let js = 'Amazing';
+console.log(45+8+23-10);
+console.log("Farhan");
+console.log(23);
+
+let firstName = "Farhan";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//Variable name conventions
+let farhan_maqsood = 'FM';
+let $function = 27;
+
+let Person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programer';
+let job2 = 'Teacher';
+
+console.log(myFirstJob);
+
+let javascriptIsFan = true;
+console.log(javascriptIsFan);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFan);
+// console.log(typeof 23);
+// console.log(typeof 'jonas');
+
+javascriptIsFan = 'YES!' 
+console.log(typeof javascriptIsFan);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age = 25;
+age = 30;
+
+const birthYear = 1996;
+// birthYear = 1991;
+
+//const job;
+
+var job = 'programer';
+job = 'teacher';
+
+lastName = 'Maqsood';
+console.log(lastName);
+
+*/
+
+const currentYear = 2037
+const farhanAge =  currentYear -1996;
+const sarahAge = currentYear - 2018;
+console.log(farhanAge , sarahAge);
+
+//mathametically operator
+console.log(currentYear - farhanAge );
+console.log(currentYear - sarahAge)
+
+console.log(farhanAge * 2 , sarahAge / 10 , 2 **3)
+// 2 **3 it mean 2 have 3 times power e,g 2*2*2
+
+const firstName = 'Farhan';
+const lastName = 'Maqsood';
+console.log(`${firstName} ${lastName}`)
+
+// Assigment operator 
+
+let x = 5 + 10 //15
+x+=10; //x = x + 10
+x*= 4; //x = x * 5
+x++; //x = x + 1
+x--;
+x--;
+
+console.log(x)
+
+//comparison operator
+console.log(sarahAge < farhanAge);
+
+
+
+
+
+
+
+
+
